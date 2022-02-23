398c2e5746e1cf6c94ce909e974d4651c7b7e30c
JD Calvelli <jdcalvelli@gmail.com>
2022-02-22 23:10:59 -0500

    add form to add new books to library and display

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 let myLibrary = [];
 
 //constructor function for books
-function Book(title, author, pages, readBool) {
+function Book(title, author, pages, readBool, isDisplayed) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readBool = readBool;
+    this.isDisplayed = isDisplayed;
 }
 
 function addBookToLibrary(title, author, pages, readBool) {
@@ -16,8 +17,25 @@ function addBookToLibrary(title, author, pages, readBool) {
 
 function displayLibraryContents() {
     myLibrary.forEach((book) => {
-        let bookDOM = document.createElement('p');
-        bookDOM.innerHTML = `${book.title} <br> ${book.author} <br> ${book.pages} <br> ${book.readBool}`;
-        document.querySelector('body').appendChild(bookDOM);
+        if(!book.isDisplayed) {
+            let bookDOM = document.createElement('p');
+            bookDOM.innerHTML = `${book.title} <br> ${book.author} <br> ${book.pages} <br> ${book.readBool}`;
+            document.querySelector('body').appendChild(bookDOM);
+            book.isDisplayed = true;
+        }
     });
 }
+
+document.querySelector('.add-book-btn').addEventListener('click', () => {
+    document.querySelector('.add-book-modal').style.display = 'block';
+});
+
+document.querySelector('#submit').addEventListener('click', () => {
+    
+    addBookToLibrary(document.querySelector('#title').value, document.querySelector('#author').value, 
+        document.querySelector('#pages').value, document.querySelector('#read').checked, false);
+    
+    displayLibraryContents();
+
+    document.querySelector('form').reset();
+});
